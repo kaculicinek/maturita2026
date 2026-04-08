9a078f13ddc5fe3b0c8b80c0ab7ec700bba81dc9
kaculicinek <kacenka@rajdlova.cz>
2026-04-08 16:48:22 +0200

    x

--- plocha-pod-krivkou/pomoci-BGE/ProjectApp/Program.cs
+++ plocha-pod-krivkou/pomoci-BGE/ProjectApp/Program.cs
@@ -7,7 +7,7 @@ internal class Program
         // Toto by měly být poslední dva řádky této Main metody -- Definují a následně spouštějí grafickou implementaci.
         //     --> Je možné definovat vlastní příkazy i "pod" těmito řádky, ale k jejich provedení dojde až po uzavření grafického okna.
         // BGEapp app = new BGEapp("BasicGraphicsEngine Application", 800, 600);
-        App app = new App("integraly", 800, 600);
+        App app = new App("Integraly", 800, 600);
         app.StartApplication();
     }
 }
